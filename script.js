a4df37117423a82c95480237f75c154053b1401e
COST_MILLAN <160264006+Cost719@users.noreply.github.com>
2025-03-20 16:30:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuicon = document.querySelector('#menu-icon');
+const navlinks = document.querySelector('.nav-links');
+
+menuIcon.oneclick = () =>{
+    navlinks.classList.toggle('active')
+}
